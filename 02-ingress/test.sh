046b7fb384342ac233b8490eddf6b7e63f87e96c
Dhrumil Mistry <56185972+dmdhrumilmistry@users.noreply.github.com>
2024-07-28 17:35:12 +0530

    fix ingress config bugs

--- 02-ingress/test.sh
+++ 02-ingress/test.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-if [ $(curl -s -o /dev/null -w "%{http_code}" http://localhost/service1) -eq 200 ]; then echo "service1 is up"; else echo "service1 is down"; fi
+if [ $(curl -k -s -o /dev/null -w "%{http_code}" https://dmdhrumilmistry.local/service1 -H "Host: dmdhrumilmistry.local") -eq 200 ]; then echo "service1 is up"; else echo "service1 is down"; fi
 
-if [ $(curl -s -o /dev/null -w "%{http_code}" http://localhost/service2) -eq 200 ]; then echo "service2 is up"; else echo "service2 is down"; fi
+if [ $(curl -k -s -o /dev/null -w "%{http_code}" https://dmdhrumilmistry.local/service2 -H "Host: dmdhrumilmistry.local") -eq 200 ]; then echo "service2 is up"; else echo "service2 is down"; fi
 
-if [ $(curl -s -o /dev/null -w "%{http_code}" http://localhost/) -eq 404 ]; then echo "success"; else echo "fail"; fi
+if [ $(curl -k -s -o /dev/null -w "%{http_code}" https://dmdhrumilmistry.local/ -H "Host: dmdhrumilmistry.local") -eq 404 ]; then echo "success"; else echo "fail"; fi
